a3cae87ce6a77cea19888c9822558d4954d9d796
divs1210 <divyansh@helpshift.com>
2018-03-27 05:14:06 +0530

    Each fn doesn't have a dedicated loop

--- src/asyncify/core.clj
+++ src/asyncify/core.clj
@@ -2,32 +2,46 @@
   (:require [clojure.core.async :as a]
             [com.climate.claypoole :as cp]))
 
+(defonce in-chan
+  (a/chan))
+
+
+(defn- execute
+  "Call `f` with `args` and put the
+  returned value on to `out-chan`."
+  [f args out-chan]
+  (let [result (try
+                 (apply f args)
+                 (catch Exception _))]
+    (if (some? result)
+      (a/put! out-chan result)
+      (a/close! out-chan))))
+
+
+(defonce event-loop
+  (a/go-loop []
+    (let [[pool f args out-chan] (a/<! in-chan)]
+      (if (= :builtin pool)
+        (a/go
+          (execute f args out-chan))
+        (cp/future
+          pool
+          (execute f args out-chan))))
+    (recur)))
+
+
 (defn asyncify
-  "Returns an asynchronous version of `f` (`af`),
+  "Returns an asynchronous version of `f`: `af`,
   that takes the same parameters,
-  but returns a channel instantly.
-  Takes one optional parameter: `n-or-threadpool`
-  that determines on which threadpool `af` will run:
-  - nil: Clojure's default agent pool
-  - number: a new threadpool of given size
-  - threadpool: given threadpool
-  (threadpool = ExecutorService or claypoole/threadpool)"
+  but returns a channel immediately.
+  `af` runs on the `core.async` threadpool,
+  unless one is explicitly passed.
+  `threadpool` = ExecutorService or claypoole/threadpool"
   ([f]
-   (asyncify f nil))
-  ([f n-or-threadpool]
-   (let [in-chan (a/chan)
-         threadpool (condp #(%1 %2) n-or-threadpool
-                      nil? :builtin
-                      number? (cp/threadpool n-or-threadpool)
-                      cp/threadpool? n-or-threadpool)]
-     (cp/future threadpool
-                (loop []
-                  (let [[out-chan args] (a/<!! in-chan)
-                        result (apply f args)]
-                    (a/put! out-chan result))
-                  (recur)))
-     (fn [& args]
-       (let [out-chan (a/chan)]
-         (a/go
-           (a/>! in-chan [out-chan args])
-           (a/<! out-chan)))))))
+   (asyncify f :builtin))
+  ([f threadpool]
+   (fn [& args]
+     (let [out-chan (a/chan)]
+       (a/go
+         (a/>! in-chan [threadpool f args out-chan])
+         (a/<! out-chan))))))
